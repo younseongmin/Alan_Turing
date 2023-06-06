029ca158cb58758034189af547e5069d887eb4a2
Seongmin Yun <k3wuendu@naver.com>
2023-06-06 15:19:48 +0900

    first commit

---
+++ as.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * as
+ */
+public class as {
+
+public static void main(String[] args) {
+
+
+        for(int i = 2; i < 10; i++) {
+            for(int j = 1; i < 10; j++) {
+                System.out.println(i + "x" + j + "=" + (i*j));
+            }
+        }
+    }
+}
